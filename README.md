5265d72e7323229b6d41e01c19fe0a005f02df4e
mind <294433706@qq.com>
2018-09-25 14:35:42 +0800

    init：项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # babel-tutorial
-babel工具的详细使用
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+browerlist 说明：
+
+```js
+{
+  "presets": [
+    ["env", {
+      "modules": false,
+      "targets": {
+        // 访问网站：http://browserl.ist/ 输入条件，查看匹配结果
+        // 条件1：返回数据按从右边到左算，首先满足 not ie <= 8，表示 排除ie8及以下办法。
+        // 条件2：返回是数据过滤是否为 最后3个版本，然后针对 ie8 进行排除。
+        // 条件3：
+        "browsers": ["> 0.5%", "last 3 versions", "not ie <= 8"]
+      }
+    }],
+    "stage-2"
+  ],
+  "plugins": ["transform-vue-jsx", "transform-runtime"]
+}
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <div @touchmove="touchMoveEvent" :class="{active:active}" @touchstart="touchStartEvent" @touchend="touchEndEvent" class="touch-wrapper">
+    </div>
+    <hr />
+    <input  @focus="onInput" value="123" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      active: false
+    }
+  },
+  methods: {
+    touchStartEvent (event) {
+      console.log(event)
+      this.active = true
+    },
+    touchEndEvent (event) {
+      console.log(event)
+      this.active = false
+    },
+    touchMoveEvent (event) {
+      console.log('touchMoveEvent')
+      console.log(event)
+    },
+    onInput (event) {
+      document.activeElement.blur()
+      this.active = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.touch-wrapper {
+  width:100px;
+  height: 100px;
+  background: green;
+}
+.active {
+  background: orange
+}
+
+input {
+  border: 1px solid #ddd;
+}
+</style>
